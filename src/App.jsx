23292b54e365a6c6979c3b107dc6aa7a4f957292
sam2k <90201071+rain2003@users.noreply.github.com>
2023-08-08 18:57:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,7 @@ import {
     useRecoilState,
     useRecoilValue,
   } from 'recoil';
-// This file shows how you can do routing in React.
-// Try going to /login, /register, /about, /courses on the website and see how the html changes
-// based on the route.
-// You can also try going to /random and see what happens (a route that doesnt exist)
+
 function App() {
     return (
         <RecoilRoot>
